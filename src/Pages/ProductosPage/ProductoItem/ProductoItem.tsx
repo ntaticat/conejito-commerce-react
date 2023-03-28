8f34f7f5fcc2a69546ece3c02eb42099909d96b2
Rafael Estrada <ntaticat@gmail.com>
2023-03-27 19:16:42 -0600

    fix productos page bugs

--- src/Pages/ProductosPage/ProductoItem/ProductoItem.tsx
+++ src/Pages/ProductosPage/ProductoItem/ProductoItem.tsx
@@ -3,24 +3,29 @@ import { Link } from "react-router-dom";
 import { IProduct } from "../../../store/slices/products/productsSlice";
 import { apiUrl } from "../../../utils/environments";
 
-export const ProductoItem = (props: IProduct) => {
+interface IProductoItem {
+  key: string;
+  product: IProduct;
+}
+
+export const ProductoItem = ({ product }: IProductoItem) => {
   const defaultImageUrl =
     "https://st4.depositphotos.com/14953852/24787/v/600/depositphotos_247872612-stock-illustration-no-image-available-icon-vector.jpg";
 
   return (
     <Link
-      to={`/productos/${props._id}`}
+      to={`/productos/${product._id}`}
       className="flex flex-nowrap justify-between items-center first:rounded-t-lg last:rounded-b-lg first:border-t-2 border-b-2 border-x-2 border-solid border-gray-900 overflow-hidden"
     >
       <div className="px-3 w-full">
-        <h2 className="text-base text-center">{props.name}</h2>
+        <h2 className="text-base text-center">{product.name}</h2>
         <div className="w-full flex flex-nowrap justify-center">
           <p className="text-xs mx-2 leading-none font-light">
-            Stock: <span className="text-sm font-normal">{props.stock}</span>
+            Stock: <span className="text-sm font-normal">{product.stock}</span>
           </p>
           <p className="text-xs mx-2 leading-none font-light">
             Precio:{" "}
-            {/* <span className="text-sm font-normal">{`$${props?.currentPrice?.amount}`}</span> */}
+            {/* <span className="text-sm font-normal">{`$${product?.currentPrice?.amount}`}</span> */}
             <span className="text-sm font-normal">{`$0`}</span>
           </p>
         </div>
@@ -29,9 +34,9 @@ export const ProductoItem = (props: IProduct) => {
         {/* <img
           className="h-16 w-32 object-cover object-center"
           src={
-            props.img === "uploads/default.jpg" || props.img === ""
+            product.img === "uploads/default.jpg" || product.img === ""
               ? defaultImageUrl
-              : `${apiUrl}/${props.img}`
+              : `${apiUrl}/${product.img}`
           }
           alt="producto"
         /> */}
--- src/Pages/ProductosPage/ProductosPage.tsx
+++ src/Pages/ProductosPage/ProductosPage.tsx
@@ -12,16 +12,14 @@ export const ProductosPage = () => {
     error,
   } = useGetProductsQuery();
 
-  const [modal, toggleModal] = useState(false);
-
-  const onToggleModal = () => toggleModal(!modal);
+  const [modal, toggleModal] = useState<boolean>(false);
 
   return (
     <PageLayout>
       {/* Productos */}
       <div className="w-full p-3">
         {productsData?.map((producto) => (
-          <ProductoItem key={producto._id} productoInfo={producto} />
+          <ProductoItem key={producto._id} product={producto} />
         ))}
       </div>
 
@@ -29,14 +27,14 @@ export const ProductosPage = () => {
       <div className="sticky left-0 bottom-0 bg-white w-full h-auto border-y-2 border-solid border-gray-900 py-2 px-3 text-center">
         <button
           className="py-2 px-3 w-full rounded-lg bg-gray-900 text-white"
-          onClick={onToggleModal}
+          onClick={() => toggleModal(!modal)}
         >
           Añadir producto
         </button>
       </div>
 
       {/* Form */}
-      <ProductoForm modal={modal} onToggleModal={onToggleModal} />
+      <ProductoForm showForm={modal} setShowForm={toggleModal} />
     </PageLayout>
   );
 };
