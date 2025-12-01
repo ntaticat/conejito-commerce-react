8473401a7274844f87f9f942800a2a78c76f4cc7
Rafael Estrada <ntaticat@gmail.com>
2025-11-30 19:33:25 -0600

    add screen-dvh extension to tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        'screen-dvh': '100dvh'
+      }
+    },
   },
   plugins: [],
 }
